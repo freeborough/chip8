44c3d649d22a179bc97aea70b9464b24ace54e7d
freeborough <andy@freeborough.com>
2023-01-17 19:57:26 +0000

    Function name change.

--- src/chip8/registers.rs
+++ src/chip8/registers.rs
@@ -2,8 +2,8 @@ use super::{Chip8, START_ADDRESS};
 
 impl Chip8 {
     /// Increments the Program Counter to the next instruction (which is 2 bytes along, as instructions are 16 bits).
-    pub fn next_instruction(self: &mut Self) {
-        self.pc = self.pc + 2;
+    pub fn increment_pc(self: &mut Self) {
+        self.pc += 2;
     }
 
     /// Resets the Program Counter to the START_ADDRESS.
