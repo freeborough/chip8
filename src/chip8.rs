b04bc8247de40e7f3458ae8c7ddd983dcffc49b9
freeborough <andy@freeborough.com>
2023-01-18 17:01:24 +0000

    Properly doc-commented struct properties.
    Switched to using the Self shorthand in new to see if it works and it does so we'll stick with that!

--- src/chip8.rs
+++ src/chip8.rs
@@ -14,19 +14,19 @@ pub struct Chip8 {
     /// Program Counter: The address in ram of the next instruction to be executed.  Defaults to START_ADDRESS.
     pub pc: u16,
 
-    // Main memory where the program, data, and font are held.
+    /// Main memory where the program, data, and font are held.
     pub ram: Vec<u8>,
 
-    // The registers v0 to vF
+    /// The registers v0 to vF
     pub v: [u8; 0xF],
 
-    // Whether or not the VM is running.
+    /// Whether or not the VM is running.
     is_running: bool,
 }
 
 impl Chip8 {
     pub fn new() -> Self {
-        Chip8 {
+        Self {
             pc: START_ADDRESS,
             ram: vec!(0; RAM_SIZE as usize),
             v: [0; 0xF],
