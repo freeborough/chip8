259ceb1f538cfaabe450e0f310b88a558401b102
freeborough <andy@freeborough.com>
2023-01-31 14:58:14 +0000

    Relative path to ROM.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
   println!("Chip8 v0.1");
 
   let mut vm = Chip8::new();
-  vm.load("/home/andy/dev/learning/rust/chip8/roms/IBM Logo.ch8");
-  vm.run();
+
+  vm.load("./roms/IBM Logo.ch8");
+  vm.run()
 }
